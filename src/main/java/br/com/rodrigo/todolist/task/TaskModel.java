873723dce0dea1d887f9f7ef18105d6b154670b8
Rodrigo Oliveira <83417184+EuJohnnyBravo@users.noreply.github.com>
2023-10-13 20:41:36 -0300

    Project

---
+++ src/main/java/br/com/rodrigo/todolist/task/TaskModel.java
@@ -0,0 +1,51 @@
+package br.com.rodrigo.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+    /*
+     *
+     * Usúario (ID_USUARIO)
+     * Descrição
+     * Título
+     * Data de Início
+     * Data de Término
+     * Prioridade
+     * 
+     */
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("O campo titulo deve ter no maximo 50 caractéres");
+        }
+
+        this.title = title;
+    }
+
+}
